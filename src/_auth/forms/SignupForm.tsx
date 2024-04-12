845c0f4546c4988852b71092ecae8a84e257dc38
pualine <prisypauline@gmail.com>
2024-04-12 12:10:32 -0700

    updating signup form

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,18 +1,19 @@
-import { FormControl, FormDescription, Form, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { FormControl, Form, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { useForm } from "react-hook-form"
 import { Button } from '@/components/ui/button'
 import { zodResolver } from "@hookform/resolvers/zod"
 import { SignupFormValidation } from "@/lib/validation"
 import { z } from "zod"
+import Loader from "@/components/shared/Loader"
 
-
-"use client"
 
 
 
 
 const SignupForm = () => {
+  // Initialize loading to true
+  const isLoading = true;
   // Define form.
   const form = useForm<z.infer<typeof SignupFormValidation>>({
     resolver: zodResolver(SignupFormValidation),
@@ -34,32 +35,76 @@ const SignupForm = () => {
   }
   return (
     <>
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <Form {...form}>
 
         <div className="sm:w-420 flex-center flex-col">
           <img src="/assets/images/logo.svg" className="mx-auto" alt="Logo" />
+
+          <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12 ">Create a new account</h2>
+          <p className="text-light-3 small-medium md:base-regular">To use TrendVibe enter your details</p>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input type="text" className="shad-input" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+             <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>username</FormLabel>
+                  <FormControl>
+                    <Input type="text" className="shad-input" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+             <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input type="email" className="shad-input" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+             <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" className="shad-input" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="shad-button_primary">
+              {isLoading?(
+                <div className="flex-center gap-2">
+                 <Loader/> Loading..
+                </div>
+              ): "Sign Up" }
+            </Button>
+          </form>
         </div>
-        
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+      </Form>
     </>
   )
 }
--- src/lib/validation/index.tsx
+++ src/lib/validation/index.tsx
@@ -5,7 +5,7 @@ import * as z from "zod"
 export const SignupFormValidation = z.object({
     name: z.string().min(2, { message: "Name too short" }),
     username: z.string().min(2, { message: "Username too short" }),
-    email: z.string().email({ message: "Invalid email" }),
+    email: z.string().email({ message: "Invalid email address" }),
     password: z.string().min(8, { message: "Password must be at least 8 characters" }),
     // confirmPassword: z.string().min(8, {message: "Password does not match"})
 
